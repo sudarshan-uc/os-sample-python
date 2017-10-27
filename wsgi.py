6bacc6e51c41b028b022f652e15c231241cd1dac
Sudarshan Chaudhari <30308970+sudarshan-uc@users.noreply.github.com>
2017-10-27 05:53:59 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello! Welcome to sudarshanchaudhari.in"
 
 if __name__ == "__main__":
     application.run()
